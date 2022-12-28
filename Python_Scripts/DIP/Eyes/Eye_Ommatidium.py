e7922bc8716775603e7cec8689650364448949d4
Abdul Wahab Butt <102326149+00awb00@users.noreply.github.com>
2022-12-28 15:32:44 +0500

    Updated Eye Omma.

--- Python_Scripts/DIP/Eyes/Eye_Ommatidium.py
+++ Python_Scripts/DIP/Eyes/Eye_Ommatidium.py
@@ -10,10 +10,13 @@ class EO_PreProcessing:
     def PreProcessing(self, orig_img):
         img = img_as_ubyte(orig_img)
         rgb_gray = color.rgb2gray(img)
-        # bins = 256
-        # img_dark = exposure.adjust_gamma(rgb_gray, gamma=3.5, gain=1)
-        # = exposure.equalize_hist(img_dark)
-        # adaptive_d_img = exposure.equalize_adapthist(img_dark, clip_limit=0.6)
-        # result_1 = unsharp_mask(adaptive_d_img, radius=5, amount=2)
+        bins = 256
+        # equalize_img = exposure.equalize_hist(rgb_gray)
+        # adaptiv_img = exposure.equalize_adapthist(rgb_gray, clip_limit=0.03)
+        # image ko set kr rahy hain
+        img_dark = exposure.adjust_gamma(rgb_gray, gamma=3.5, gain=1)
+        equalized_d_img = exposure.equalize_hist(img_dark)
+        adaptive_d_img = exposure.equalize_adapthist(img_dark, clip_limit=0.6)
+        result_1 = unsharp_mask(adaptive_d_img, radius=5, amount=2)
 
-        return rgb_gray
+        return adaptive_d_img, rgb_gray, result_1
