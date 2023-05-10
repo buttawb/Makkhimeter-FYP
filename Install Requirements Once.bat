471600727f1f29c9321959bbbd7ea6b8ffb5c643
AWB <leo.awb@gmail.com>
2023-05-10 14:36:09 +0500

    Added VENV support.

--- Install Requirements Once.bat
+++ Install Requirements Once.bat
@@ -1,7 +1,21 @@
 @echo off
+
+set VENV_DIR=venv
+
+if exist %VENV_DIR% (
+  echo Awaking PYTHON...
+  call %VENV_DIR%\Scripts\activate.bat
+) else (
+  echo Creating virtual environment...
+  python -m venv %VENV_DIR%
+  echo Virtual environment created.
+  echo Awaking PYTHON...
+  call %VENV_DIR%\Scripts\activate.bat
+)
+
+
+
 echo Installing dependencies...
-call venv\Scripts\activate.bat
-echo Awaking PYTHON ...
 
 pip install -r requirements.txt
 echo Requirements installation completed.
